c5e9e0c12d0fd9ecabe3ac3fb707076018b6fb1f
snakepatch53 <snakepatch53@gmail.com>
2023-05-10 18:25:18 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   public/css.general/theme1.css
    	modified:   public/css.public/frontpage.css
    	modified:   public/css.public/home.css
    	new file:   public/img/car.jpg
    	new file:   public/img/stars.svg
    	modified:   src/templates/public.pages/home.php

--- src/templates/public.pages/home.php
+++ src/templates/public.pages/home.php
@@ -18,13 +18,54 @@
         <section class="frontpage">
             <?php include('./src/templates/public.component/frontpage.php') ?>
         </section>
+
         <section class="section-1">
-            <div class="container">
-                <h2>An easier way to build</h2>
-                <p>Our experienced team is here to help with the entire process, from initial idea to commissioning, we are here to help you get the design and work you need.</p>
+            <div class="subsection">
+                <div class="container">
+                    <h2>An easier way to build</h2>
+                </div>
+            </div>
+            <div class="container banner">
+                <img class="bg-img" src="<?= $DATA['http_domain'] ?>public/img/car.jpg" alt="Image of car in the road">
+                <div class="items">
+                    <div class="item"></div>
+                    <div class="item">
+                        <p>Our experienced team is here to help with the entire process, from initial idea to commissioning, we are here to help you get the design and work you need.</p>
+                    </div>
+                </div>
             </div>
         </section>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+        <section class="section-2">
+            <div class="container">
+                <h2>Qualities of our brand</h2>
+                <div class="items">
+                    <div class="item">
+                        <div class="icon">
+                            <?php include('./public/img/stars.svg'); ?>
+                        </div>
+                        <h3>Innovation</h3>
+                        <p>Find the best details and always look up to date</p>
+                    </div>
+                    <div class="item">
+                        <div class="icon">
+                            <?php include('./public/img/stars.svg'); ?>
+                        </div>
+                        <h3>Commitment</h3>
+                        <p>You'll have our guarantee in quality and punctuality</p>
+                    </div>
+                    <div class="item">
+                        <div class="icon">
+                            <?php include('./public/img/stars.svg'); ?>
+                        </div>
+                        <h3>Elegance</h3>
+                        <p>Up to date with all current aesthetic details</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
     </main>
