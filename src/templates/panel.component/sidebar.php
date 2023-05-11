e08d0ad646af65f5f088d86719d19dd7d91906d1
snakepatch53 <snakepatch53@gmail.com>
2023-05-11 17:26:38 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/templates/panel.component/sidebar.php

--- src/templates/panel.component/sidebar.php
+++ src/templates/panel.component/sidebar.php
@@ -14,52 +14,22 @@
 
         <a href="<?= $DATA['http_domain'] ?>panel/" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "home") ? "shadow  active" : "" ?>">
             <i class="fa-solid fa-house"></i>
-            <span class="ms-2">Inicio</span>
+            <span class="ms-2">Home</span>
         </a>
 
         <a href="<?= $DATA['http_domain'] ?>panel/info" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "info") ? "shadow  active" : "" ?>">
             <i class="fa-solid fa-info"></i>
-            <span class="ms-2">Informacion</span>
+            <span class="ms-2">Information</span>
         </a>
 
         <a href="<?= $DATA['http_domain'] ?>panel/users" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "users") ? "shadow  active" : "" ?>">
             <i class="fa-solid fa-users"></i>
-            <span class="ms-2">Usuarios</span>
-        </a>
-
-        <a href="<?= $DATA['http_domain'] ?>panel/links" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "links") ? "shadow  active" : "" ?>">
-            <i class="fa-solid fa-link"></i>
-            <span class="ms-2">Links</span>
+            <span class="ms-2">Users</span>
         </a>
 
         <a href="<?= $DATA['http_domain'] ?>panel/mensajes" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "mensajes") ? "shadow  active" : "" ?>">
             <i class="fa-solid fa-envelope"></i>
-            <span class="ms-2">Mensajes</span>
-        </a>
-
-        <a href="<?= $DATA['http_domain'] ?>panel/planes" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "planes") ? "shadow  active" : "" ?>">
-            <i class="fa-solid fa-credit-card"></i>
-            <span class="ms-2">Planes</span>
-        </a>
-
-        <a href="<?= $DATA['http_domain'] ?>panel/preguntas" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "preguntas") ? "shadow  active" : "" ?>">
-            <i class="fa-solid fa-question"></i>
-            <span class="ms-2">Preguntas</span>
-        </a>
-
-        <a href="<?= $DATA['http_domain'] ?>panel/restricciones" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "restricciones") ? "shadow  active" : "" ?>">
-            <i class="fa-solid fa-lock"></i>
-            <span class="ms-2">Restricciones</span>
-        </a>
-
-        <a href="<?= $DATA['http_domain'] ?>panel/slider" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "slider") ? "shadow  active" : "" ?>">
-            <i class="fa-solid fa-images"></i>
-            <span class="ms-2">Slider</span>
-        </a>
-
-        <a href="<?= $DATA['http_domain'] ?>panel/social" class="nav-option btn btn-outline-primary border-0 text-start p-3 mb-2 <?= ($DATA['name'] == "social") ? "shadow  active" : "" ?>">
-            <i class="fa-solid fa-share"></i>
-            <span class="ms-2">Redes</span>
+            <span class="ms-2">Messages</span>
         </a>
 
 
