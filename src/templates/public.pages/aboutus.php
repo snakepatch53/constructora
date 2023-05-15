68160cc6952fcbdca1ef38d4aa5fca7a3592b77e
snakepatch53 <snakepatch53@gmail.com>
2023-05-15 17:19:45 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   public/css.public/aboutus.css
    	new file:   public/img.team/1.png
    	new file:   public/img.team/2.png
    	new file:   public/img.team/3.png
    	new file:   public/img.team/4.png
    	new file:   public/img.team/default.png
    	new file:   public/img/frontpage-aboutus.jpg
    	new file:   public/img/frontpage-services.jpg
    	modified:   src/templates/public.pages/aboutus.php

--- src/templates/public.pages/aboutus.php
+++ src/templates/public.pages/aboutus.php
@@ -13,8 +13,85 @@
     </header>
 
     <main class="animate__animated animate__fadeIn">
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <section class="frontpage">
+            <div class="bg-img">
+                <img src="<?= $DATA['http_domain'] ?>public/img/frontpage-aboutus.jpg" alt="Imagen de portada de la pagina">
+            </div>
+            <div class="container">
+                <h1>Our brand, our company, our ideaology</h1>
+                <a href="#section2">
+                    <i class="fas fa-arrow-down"></i>
+                </a>
+            </div>
+        </section>
+
+        <section class="section2" id="section2">
+            <div class="container">
+                <h2>Mission & Vision</h2>
+                <div class="items">
+                    <div class="item">
+                        <h3>Mission</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                    <div class="item">
+                        <h3>Vision</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section3">
+            <div class="container">
+                <h2>Goals with you</h2>
+                <div class="items">
+                    <div class="item">
+                        <div class="icon">
+                            <?php include('./public/img/stars.svg'); ?>
+                            <i class="fa fa-hand-holding-heart"></i>
+                        </div>
+                        <h3>Advise</h3>
+                    </div>
+
+                    <div class="item">
+                        <div class="icon">
+                            <?php include('./public/img/stars.svg'); ?>
+                            <i class="fa fa-handshake"></i>
+                        </div>
+                        <h3>Escort</h3>
+                    </div>
+
+                    <div class="item">
+                        <div class="icon">
+                            <?php include('./public/img/stars.svg'); ?>
+                            <i class="fa fa-lightbulb"></i>
+                        </div>
+                        <h3>Innovate</h3>
+                    </div>
+                </div>
+                <p>Our commitment is to provide you with the best experience so that you can fulfill your desire to renovate your home.</p>
+            </div>
+        </section>
+
+
+
+        <section class="section4">
+            <div class="container">
+                <h2>Our team</h2>
+                <div class="items">
+                    <?php for ($i = 1; $i < 5; $i++) { ?>
+                        <a href="#" class="item" target="_blank">
+                            <div class="img">
+                                <img src="<?= $DATA['http_domain'] ?>public/img.team/<?= $i ?>.png" alt="Image of team member">
+                            </div>
+                            <h3>Member #<?= $i ?></h3>
+                            <span>Position of menber #<?= $i ?></span>
+                        </a>
+                    <?php } ?>
+                </div>
+                <p>Our commitment is to provide you with the best experience so that you can fulfill your desire to renovate your home.</p>
+            </div>
+        </section>
     </main>
 
     <footer id="section-footer">
